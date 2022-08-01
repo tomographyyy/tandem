95192c0cc71d5c7a659878c1f4a1960e9e0fe408
tomographyyy <34155315+tomographyyy@users.noreply.github.com>
2022-08-01 18:01:03 +0900

    Delete tandem_stag directory

--- tandem_stag/__init__.py
+++
@@ -1,6 +0,0 @@
-# mymodule/__init__.py
-from .okada import Okada
-from .ocean import Ocean  # mymodule.csvfile.CsvFile --> mymodule.CsvFile でアクセス可能にする
-from .dmstag import DMStagDA, DMDAHierarchy, DMStagVariableSet, DMStagBase
-from .solidearth import SolidEarth
-from .tandem import Tandem
