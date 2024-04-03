71af5ab5b31c7040e763d03dcb41f2a876248276
tomographyyy <takagawatomohiro@gmail.com>
2024-04-03 15:12:15 +0900

    setup.py for only Okada

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tandem',
+    version="0.0.1",
+    description="Tsunami Adjoint Simulator",
+    author='Tomohiro TAKAGAWA',  # 作者名
+    packages=find_packages(),  # 使うモジュール一覧を指定する
+    license='Apache-2.0'  # ライセンス
+)
--- src/tandem/__init__.py
+++ src/tandem/__init__.py
@@ -1,6 +1,5 @@
-# mymodule/__init__.py
 from .okada import Okada
-from .ocean import Ocean  # mymodule.csvfile.CsvFile --> mymodule.CsvFile でアクセス可能にする
-from .dmstag import DMStagDA, DMDAHierarchy, DMStagVariableSet, DMStagBase
-from .solidearth import SolidEarth
-from .tandem import Tandem
+#from .ocean import Ocean 
+#from .dmstag import DMStagDA, DMDAHierarchy, DMStagVariableSet, DMStagBase
+#from .solidearth import SolidEarth
+#from .tandem import Tandem
