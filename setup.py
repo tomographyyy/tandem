dd3949612e5242afc10bd4bd7a1c6032ff01f017
Tomohiro TAKAGAWA <34155315+tomographyyy@users.noreply.github.com>
2024-04-03 16:35:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+def load_requires_from_file(filepath):
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
 setup(
     name='tandem',
     version="0.0.1",
     description="Tsunami Adjoint Simulator",
-    author='Tomohiro TAKAGAWA',  # 作者名
-    packages=find_packages(),  # 使うモジュール一覧を指定する
-    license='Apache-2.0'  # ライセンス
+    author='Tomohiro TAKAGAWA',
+    packages=find_packages(),
+    license='Apache-2.0'
+    install_requires=load_requires_from_file("requirements.txt")
 )
