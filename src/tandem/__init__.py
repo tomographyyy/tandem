2eaf9a7837de45086931ee6f767d0a6a7321770e
tomographyyy <takagawatomohiro@gmail.com>
2024-04-03 16:13:13 +0900

    delete old src

--- src/tandem/__init__.py
+++
@@ -1,5 +0,0 @@
-from .okada import Okada
-#from .ocean import Ocean 
-#from .dmstag import DMStagDA, DMDAHierarchy, DMStagVariableSet, DMStagBase
-#from .solidearth import SolidEarth
-#from .tandem import Tandem
