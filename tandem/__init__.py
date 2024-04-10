8bc3c2077a2674df60db441dce82ff1771bb0db9
tomographyyy <takagawatomohiro@gmail.com>
2024-04-10 10:49:59 +0900

    add dmstag.py

--- tandem/__init__.py
+++ tandem/__init__.py
@@ -1,4 +1,5 @@
 from .okada import Okada
 from .decompi import DecoratorMPI
 from .xslice import SliceEx
-__all__ = ["Okada", "DecoratorMPI", "SliceEx"]
+from .dmstag import DMStagDA, DMDAHierarchy, DMStagVariableSet, DMStagBase
+#__all__ = ["Okada", "DecoratorMPI", "SliceEx"]
