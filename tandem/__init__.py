671822032557f25d5305def7a544bd4f6cefe8a8
tomographyyy <takagawatomohiro@gmail.com>
2024-04-10 18:43:54 +0900

    update __init__.py

--- tandem/__init__.py
+++ tandem/__init__.py
@@ -3,4 +3,4 @@ from .decompi import DecoratorMPI
 from .xslice import SliceEx
 from .dmstag import DMStagDA, DMDAHierarchy, DMStagVariableSet, DMStagBase
 from .station import Angle, Station
-#__all__ = ["Okada", "DecoratorMPI", "SliceEx"]
+__all__ = ["Okada", "DecoratorMPI", "SliceEx",]
