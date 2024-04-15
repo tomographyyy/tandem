f63d161ec554ff06aa9f212e654aa7d3279f646c
tomographyyy <takagawatomohiro@gmail.com>
2024-04-15 18:45:25 +0900

    update gitignore

--- tandem/generate_settings.py
+++ tandem/generate_settings.py
@@ -1,7 +1,10 @@
 import json
 
 settings_filename = "settings.json"
-settings = dict(outpath="out")
+settings = dict(outpath="outdir=output_0101-1234567-forward_C1S1B1GR030_Sheehan_Manning0.025_NL",
+                job_id="1234567",
+                job_name="job_name",
+                )
 
 def generate():
     with open(settings_filename, "w") as f:
