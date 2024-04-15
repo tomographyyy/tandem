54740d5e36b57de7cecd05f76e538f08b7cda136
tomographyyy <takagawatomohiro@gmail.com>
2024-04-15 16:57:12 +0900

    update generate_settings.py

--- tandem/generate_settings.py
+++ tandem/generate_settings.py
@@ -3,8 +3,11 @@ import json
 settings_filename = "settings.json"
 settings = dict(outpath="out")
 
-
-if __name__=="__main__":
+def generate():
     with open(settings_filename, "w") as f:
         json.dump(settings, settings_filename, indent=2)
-    print(f"{settings_filename} is generated.")
+    print(f"{settings_filename} is generated.")    
+
+
+if __name__=="__main__":
+    generate()
